9da2fa8e4c323dd5f9a5fbb2f6b4b858a01d0f2e
Andreas Eismann <andreas@eismaenners.de>
2020-05-22 23:52:44 +0200

    0

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const GitHub = require('github-api');
-const fs = require('fs');
+// const GitHub = require('github-api');
+// const fs = require('fs');
 
-const gh = new GitHub(
-    fs.readFileSync('config.json').toJSON()
-);
+// const gh = new GitHub(
+//     fs.readFileSync('config.json').toJSON()
+// );
 
-const repo = gh.getRepo('abulvenz/contribution-pixel-messages');
+// const repo = gh.getRepo('abulvenz/contribution-pixel-messages');
 
-repo.listCommits({}, (err, commits) => {
-    (commits.map(c => console.log(c.sha)))
-})
+// repo.listCommits({}, (err, commits) => {
+//     console.log(commits[0].sha);
+//     repo.commit()
+// });
+
+const { spawn } = require('child_process');
+const add = spawn('git', ['add', '.']);
+
+add.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+
+const i = 0;
+
+const commit = spawn('git', ['commit', '-m', "" + i]);
